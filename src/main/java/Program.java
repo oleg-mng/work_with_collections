9d63425d02f94391c4ab71ea1c11f7083291c100
oleg <legik7127@gmail.com>
2023-04-15 22:52:31 +0600

    Initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Program {
+    public static void main(String[] args) {
+        LinkedList<Integer> lList= new LinkedList<>();
+        lList.add(123);
+        lList.add(456);
+        lList.add(789);
+        System.out.println(lList);
+    }
+}
