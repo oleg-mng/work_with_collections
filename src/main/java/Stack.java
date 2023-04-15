733266b0d3bc5919ff45ecc7091cdf31fe2a2809
oleg <legik7127@gmail.com>
2023-04-16 00:10:27 +0600

    added Stack<Integer>

---
+++ src/main/java/Stack.java
@@ -0,0 +1,53 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+public class Stack<I extends Number> {
+    public static void main(String[] args) {
+        var exp = "2 1 2 3 * + ^".split(" ");
+        int res = 0;
+        System.out.println(Arrays.toString(exp));
+
+        java.util.Stack<Integer> st = new java.util.Stack<>();
+        for (int i = 0; i < exp.length; i++) {
+
+            if (isDigit(exp[i])) {
+                st.push(Integer.parseInt(exp[i]));
+            } else {
+                switch (exp[i]) {
+                    case "+":
+                        res = st.pop() + st.pop();
+                        st.push(res);
+                        break;
+                    case "-":
+                        res = -st.pop() + st.pop();
+                        st.push(res);
+                        break;
+                    case "*":
+                        res = st.pop() * st.pop();
+                        st.push(res);
+                        break;
+                    case "/":
+                        res =  st.pop()/ st.pop();
+                        st.push(res);
+                        break;
+                    case "^":
+                        res =  st.pop()^ st.pop();
+                        st.push(res);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        System.out.printf("%d\n", st.pop());
+    }
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
