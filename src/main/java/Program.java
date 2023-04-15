65d469c19d271dcdeb248a29eebedc843d2646e5
oleg <legik7127@gmail.com>
2023-04-15 23:01:16 +0600

    added Queue<Integer>

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,21 @@
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Program {
     public static void main(String[] args) {
         LinkedList<Integer> lList= new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
         lList.add(123);
         lList.add(456);
         lList.add(789);
+
+        queue.add(7);
+        queue.add(99);
+        queue.remove();
+        queue.add(107);
+
         System.out.println(lList);
+        System.out.println(queue);
     }
 }
