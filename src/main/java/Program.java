99066e6bb7ed1c30089b51f8e58c4dfe61b22c89
oleg <legik7127@gmail.com>
2023-04-15 23:26:17 +0600

    added PDeque<Integer>

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,12 +1,11 @@
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
         LinkedList<Integer> lList= new LinkedList<>();
         Queue<Integer> queue = new LinkedList<>();
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        Deque<Integer> deque = new ArrayDeque<>();
 
         lList.add(123);
         lList.add(456);
@@ -22,8 +21,20 @@ public class Program {
         priorityQueue.add(88);
         priorityQueue.poll();
 
+        deque.addFirst(5);
+        deque.addLast(7);
+        deque.removeLast();
+        deque.offerFirst(777);
+        deque.offerLast(555);
+        deque.offer(0);
+//        deque.pollLast();
+//        deque.getLast();
+
+
         System.out.println(lList);
         System.out.println(queue);
         System.out.println(priorityQueue);
+        System.out.println(deque);
+        System.out.println(deque.peekLast());
     }
 }
