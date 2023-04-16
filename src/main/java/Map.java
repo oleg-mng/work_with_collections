a04a6bf75447728021b827e79a0c9713c1efcd3b
oleg <legik7127@gmail.com>
2023-04-16 23:07:08 +0600

    use Map<Integer, String>

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.logging.SocketHandler;
+
 public class Map {
     public static void main(String[] args) {
         Integer a = 33;
+        java.util.Map<Integer, String> hmp = new HashMap<>();
+
+        hmp.putIfAbsent(1, "Карта города");
+
+
         System.out.println(a.hashCode());
+        System.out.println("z".hashCode());
+
+        System.out.println(hmp);
     }
 }
