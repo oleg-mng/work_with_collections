fd5aadac62a0e8e6af085d05b0f9aeafa3ad2971
oleg <legik7127@gmail.com>
2023-04-18 22:39:53 +0600

    added collection TreeMap<Integer, String>

---
+++ src/main/java/TreeMap.java
@@ -0,0 +1,18 @@
+public class TreeMap {
+
+    public static void main(String[] args) {
+        java.util.TreeMap<Integer, String> treeMap = new java.util.TreeMap<>();
+
+        treeMap.put(1, "one");
+        System.out.println(treeMap);
+        treeMap.put(5, "five");
+        System.out.println(treeMap);
+        treeMap.put(3, "three");
+        System.out.println(treeMap);
+        treeMap.put(9, "nine");
+        System.out.println(treeMap);
+        treeMap.put(7, "seven");
+        System.out.println(treeMap);
+
+    }
+}
