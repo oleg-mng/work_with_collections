7ed36846108aaaf76b217e4d41f8836a4d2c5c6a
oleg <legik7127@gmail.com>
2023-04-20 00:13:35 +0600

    added methods addAll(), retainAll(), removeAll()

--- src/main/java/HashSet.java
+++ src/main/java/HashSet.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Set;
 
 public class HashSet {
@@ -17,5 +18,20 @@ public class HashSet {
         hashSet.clear();
         System.out.println();
         System.out.println(hashSet);
+
+        var a = new java.util.HashSet<>(Arrays.asList(1, 3, 4, 5, 6, 7, 8, 9));
+        var b = new java.util.HashSet<>(Arrays.asList(7, 8, 9, 11, 12, 14));
+        var u = new java.util.HashSet<>(a);
+        u.addAll(b);
+        var v = new java.util.HashSet<>(a);
+        v.retainAll(b);
+        var s = new java.util.HashSet<>(a);
+        s.removeAll(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(u);
+        System.out.println(v);
+        System.out.println(s);
+
     }
 }
