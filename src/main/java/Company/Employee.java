ebc36631f3fe851e68542bde4601704e12b39bec
oleg <legik7127@gmail.com>
2023-04-21 22:28:55 +0600

    added method hashCode()

--- src/main/java/Company/Employee.java
+++ src/main/java/Company/Employee.java
@@ -1,24 +1,35 @@
 package Company;
 
 public class Employee {
-        Integer id;
-        String firstName;
-        String lastName;
-        Integer age;
-        String profession;
+    Integer id;
+    String firstName;
+    String lastName;
+    Integer age;
+    String profession;
 
-        public Employee(Integer id, String firstName, String lastName, Integer age, String profession) {
-            this.id = id;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.age = age;
-            this.profession = profession;
-        }
+    public Employee(Integer id, String firstName, String lastName, Integer age, String profession) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.profession = profession;
+    }
 
     @Override
     public String toString() {
-            String res = String.format("%d %s %s %d %s", id, firstName, lastName, age, profession);
+        String res = String.format("%d %s %s %d %s", id, firstName, lastName, age, profession);
         return res;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Employee t = (Employee) o;
+        return id == t.id && firstName == t.firstName;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
 
