9fd97e1af8c1bba8fd321479360ab406b3eb4c8c
oleg <legik7127@gmail.com>
2023-04-21 22:33:33 +0600

    added method equals(Object o)

--- src/main/java/Company/Main.java
+++ src/main/java/Company/Main.java
@@ -10,7 +10,10 @@ public class Main {
         Employee w1 = new Employee(1, "Petr", "Krikunov", 32, "developer");
         Employee w2 = new Employee(2, "Varvara", "Goncharova", 35, "developer");
         Employee w3 = new Employee(3, "Jury", "Marshak", 41, "developer");
+        Employee w4 = new Employee(1, "Petr", "Krikunov", 32, "developer");
         System.out.println(w1.toString());
+        System.out.println(w2.hashCode());
+        System.out.println(w1.equals(w4));
 
         var m = new HashSet<>(Arrays.asList(w1, w2, w3));
         System.out.println(m);
